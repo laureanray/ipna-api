4953c8bb3ed6e1fcb1750300408d4fabf353f3f3
laureanray <laureanraybahala@gmail.com>
2020-11-29 21:03:12 +0800

    added test case for 404s

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -5,6 +5,21 @@ require('tap').mochaGlobals()
 
 const build = require('../app')
 
+describe('/ Tests', async () => {
+  const app = build()
+
+  context('when sending requests to non existent endpoint(s)', async () => {
+    it('returns a 404 status code', async () => {
+      const response = await app.inject({
+        method: 'GET',
+        url: '/test'
+      })
+
+      response.statusCode.should.equal(404)
+    })
+  })
+})
+
 describe('/v1/:name Tests', async () => {
   const app = build()
 
