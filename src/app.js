959c2b9382b5d3f99dd330e4a26f6bc3baf4422d
laureanray <laureanraybahala@gmail.com>
2020-11-30 22:48:44 +0800

    added fastify-rate-limit dep and updated app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const fastify = require('fastify')
 
 function build (opts = {}) {
   const app = fastify(opts)
+  app.register(require('fastify-rate-limit'), {
+    max: 30,
+    timeWindow: '1 minute'
+  })
   app.register(require('./routes/v1/ipna'), { prefix: '/v1' })
   return app
 }
